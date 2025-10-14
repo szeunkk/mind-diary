21ee0073873f65123e50ab39906604471a6c457e
szeunkk <szeunkk@gmail.com>
2025-10-14 15:51:57 +0900

    storybook import 수정

--- src/commons/components/searchbar/index.stories.tsx
+++ src/commons/components/searchbar/index.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/nextjs";
 import SearchBar from "./index";
 
 const meta: Meta<typeof SearchBar> = {
